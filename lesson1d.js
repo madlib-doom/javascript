1bd564261a2033e450d6838a34e1ad0c93e2197c
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-27 12:48:20 +0300

    uPDATE

---
+++ lesson1d.js
@@ -0,0 +1,25 @@
+// integer ~ whole number
+
+let age=49
+console.log("Your are",age,"years old")
+
+// floating point ~number with decimal
+
+let quantity=40.982
+console.log("The quantity is:",quantity)
+
+// Boolean~T or F
+let isAdult=true;
+let isRegistered=false;
+
+console.log("Person an adult?",isAdult)
+console.log("Is he registered?",isRegistered)
+
+// undefined ~ Data type that is declared without assigning any values
+
+let undefined;
+console.log(undefined)
+
+// null ~ Data type that contains null values
+let user =null;
+console.log(user)
