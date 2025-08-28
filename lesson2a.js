d66002406140c948912ffa50ee4858f03d46ac3e
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-28 11:13:56 +0300

    update code

---
+++ lesson2a.js
@@ -0,0 +1,23 @@
+// Javascript object data types
+
+let person={
+    name:"Joseph william",
+    age:53,
+    isTeacher:true
+
+}
+
+console.log("The details of the person are:",person)
+
+// we use the dot separator to access specific values of an object.
+console.log("The name of the person is:",person.name)
+console.log("The age of the person is:",person.age)
+console.log("Is the person  a TEACHER:",person.isTeacher)
+
+// Array datatype:
+// Refers to a collection of items that are on indexes
+
+fruits=["Apple","mango","banana","lemon","tomato","grapes","oranges","berries"]
+
+console.log("The list of fruits is:",fruits)
+console.log("A green fruit is:",fruits[3])
