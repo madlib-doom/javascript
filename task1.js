f8a52b6fa0d516ae2b39de6bbd71e74437e0fb6c
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-28 11:32:21 +0300

    update code

---
+++ task1.js
@@ -0,0 +1,10 @@
+car={
+    brand:"FORD",
+    model:"GT",
+    color:"mateblack",
+    horsepower:"600horsepower"
+}
+console.log("the strength of this car is:",car.horsepower)
+
+mycars=["alfa romeo","aston martin","ford","koinesagg","lambo","porsche"]
+console.log("The fastets car is:",mycars[3])
