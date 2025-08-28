fa542ce96f2195de0ea359794640252740f3f55d
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-28 12:41:11 +0300

    update code

---
+++ lesson2c.js
@@ -0,0 +1,12 @@
+// if conditional statements
+// used in control flow execution structure of a program 
+// executes a block of code /statement if condition is met 
+
+let age=28;
+
+if(age < 18){
+    console.log("You are too young to watch this movie..")
+}
+else{
+    console.log("sit down and watch the episode...")
+}
---
+++ lesson2d.js
@@ -0,0 +1,16 @@
+// if..elseif..else statement
+// used to evaluate multiple conditions 
+  let age =-2
+  if (age<15) {
+    console.log("You are too young to watch this episode.")
+  }
+  else if(age>=15 && age<=18){
+    console.log("You can watch it with parental guidance")
+
+  }
+  else if (age>=18 && age<40) {
+    console.log("Have a seat and enjoy the movie")
+  }
+  else{
+    console.log("This movie does not suite you")
+  }
