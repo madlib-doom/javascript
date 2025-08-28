13418933ff5b2f76ccf634c2bc47ec27956d2b73
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-28 11:59:29 +0300

    update code

---
+++ lesson2b.js
@@ -0,0 +1,27 @@
+//comparison operators
+//They give a boolean value
+
+let number1=10
+let number2=20
+
+console.log(number1<number2)
+console.log(number1>number2)
+console.log(number1<=number2)
+console.log(number1 >= number2)
+console.log(number1==number2)
+console.log(number1 != number2)
+console.log(number1 === number2)
+
+//logical operators
+// logical AND(&&)
+// evaluates to true if both values are true 
+
+console.log(number1<number2 && number2>number1)
+
+//logical OR(|)
+// EVALUTAES TO TRUE IF ONE OF THE STATAMENTS IS TRUE 
+console.log((number1<number2)||(number1>number2))
+
+// logical not(!)
+// negates (gives opposite of something)
+console.log(!(number1 > number2))
