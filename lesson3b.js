6462b0add66912803982983a80dd82cb74406677
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-29 12:00:12 +0300

    update code

---
+++ lesson3b.js
@@ -0,0 +1,18 @@
+// while loop 
+// used when the length of items is not known 
+// structure :
+// 1.initialization of variable 
+// while keyword 
+// condition to be checked 
+// increment/decrement 
+
+let i=0;
+while(i<=10){
+    console.log(i)
+    i++
+}
+let num=10;
+while(i>=1){
+    console.log(i)
+    i--
+}
