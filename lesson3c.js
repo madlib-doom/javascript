a84a9b34614d69b1c4face734a6c6fc7b3a25c95
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-29 12:09:09 +0300

    update code

---
+++ lesson3c.js
@@ -0,0 +1,11 @@
+const numbers=[1,2,3,4,5]
+
+const double=numbers.map(num => num *2)
+
+console.log(double)
+
+const names=["John","Peter","Andrew","Aaron","Ann"]
+
+const greetings=names.map(name=>"Hello,"+name)
+
+console.log(greetings)
