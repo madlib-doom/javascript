6c4da365acae4c89e217f3e7ab33a20bd9fa6dbe
madlib-doom~ <aaronplimo021@gmail.com>
2025-08-29 11:51:12 +0300

    update code

---
+++ lesson3a.js
@@ -0,0 +1,42 @@
+// for loop 
+// are used to iterate through something 
+// structure of for loop 
+// 1.start startvalue, 
+// 2.condition 
+// 3.increment 
+    // Body~statement to be printed out 
+
+    // for(let i=0;i<=10;i++){
+    //     console.log(i)
+    // }
+
+    // let fruits=["Apple","mango","banana","lemon","tomato","grapes","oranges","berries"]
+
+    // arr=fruits.length
+
+    // for (i=0;i<arr;i++){
+    //     console.log(i)
+    // }
+ 
+    
+    for(let i=1970;i<=2021;i++){
+     if(i % 4==0){
+        console.log(i,"is a leap year.")
+     }
+     else{
+        console.log(i,"not a leap year")
+     }
+    }
+
+    for(i=10;i>=1;i--){
+        if (i==5){
+            console.log("The number is 5")
+            
+        }
+       else{
+        console.log(i)
+       }
+
+      }
+
+   
