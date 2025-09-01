b7387d1175ede3c6e21dbfea5c9bf37b56e16226
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-01 11:28:49 +0300

    update code

---
+++ lesson4a.js
@@ -0,0 +1,11 @@
+// JS functions
+function greetings(){
+    console.log("Hello there.Hope you are fine.Nice to meet you...")
+}
+// call/invoke  the function by its name
+greetings()
+
+function going(){
+    console.log("Goodbye.Nice to have met you.")
+}
+going()
