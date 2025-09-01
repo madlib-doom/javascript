18a2a9e116fbb3de2b4acd56bea1bc371cbc5982
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-01 11:15:18 +0300

    update code

--- lesson3b.js
+++ lesson3b.js
@@ -6,13 +6,31 @@
 // condition to be checked 
 // increment/decrement 
 
-let i=0;
-while(i<=10){
-    console.log(i)
+// let i=0;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+// let num=10;
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+let i=1797
+while(i<1810){
+    if (i%4==0 )
+    {
+        console.log("The leap year is:",i)
+    }
+    else if(i%400==0){
+console.log("The year is a leap year")
+    }
+    else if(i%100==0){
+console.log(i)
+    }
+    else{
+        console.log(i)
+    }
+   
     i++
-}
-let num=10;
-while(i>=1){
-    console.log(i)
-    i--
 }
